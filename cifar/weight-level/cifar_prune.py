dabd5a27def23044986d8e1dfc74aa224beee323
x-zho14ls <18813119938@163.com>
2020-06-19 16:13:10 +0800

    a

--- cifar/weight-level/cifar_prune.py
+++ cifar/weight-level/cifar_prune.py
@@ -101,12 +101,12 @@ def main():
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize(mean=[0.491, 0.482, 0.447], std=[0.247, 0.243, 0.262]),
     ])
 
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize(mean=[0.491, 0.482, 0.447], std=[0.247, 0.243, 0.262]),ß
     ])
     if args.dataset == 'cifar10':
         dataloader = datasets.CIFAR10
