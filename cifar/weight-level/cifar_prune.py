b1054c1e10fae1fe0eea3fe0cae852391c01d475
x-zho14ls <18813119938@163.com>
2020-06-19 14:22:37 +0800

    Update cifar_prune.py

--- cifar/weight-level/cifar_prune.py
+++ cifar/weight-level/cifar_prune.py
@@ -202,11 +202,11 @@ def main():
     for k, m in enumerate(model.modules()):
         if isinstance(m, nn.Conv2d):
             weight_copy = m.weight.data.abs().clone()
-            weight_copy_2 = m.weight.data.view(-1).abs().clone()
-            if True:
-                y, i = torch.sort(weight_copy_2)
-                thre_index = int(m.weight.data.numel() * args.percent)
-                thre = y[thre_index]
+            # weight_copy_2 = m.weight.data.view(-1).abs().clone()
+            # if True:
+            #     y, i = torch.sort(weight_copy_2)
+            #     thre_index = int(m.weight.data.numel() * args.percent)
+            #     thre = y[thre_index]
             mask = weight_copy.gt(thre).float().cuda()
             pruned = pruned + mask.numel() - torch.sum(mask)
             m.weight.data.mul_(mask)
